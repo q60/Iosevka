6bfa1c8b176689e1b8699ba0bbedba345fae7ed8
kira malinova <llathasa@outlook.com>
2025-02-25 09:26:22 +0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "custom Iosevka build";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) buildNpmPackage;
+    in {
+      packages = {
+        default = buildNpmPackage rec {
+          name = "iosevka-q60";
+
+          nativeBuildInputs = with pkgs; [
+            ttfautohint
+          ];
+
+          src = ./.;
+
+          npmDepsHash = "sha256-TOFCTYReO7eNBv1udG1Ie3Zww3uOVPFTo0x0jwyCAHE=";
+
+          buildPhase = ''
+            runHook preBuild
+            npm run build -- ttf::IosevkaCode --jCmd=$NIX_BUILD_CORES
+            npm run build -- ttf::IosevkaTerminal --jCmd=$NIX_BUILD_CORES
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            fontdir="$out/share/fonts/truetype"
+            install -d "$fontdir"
+            install "dist/IosevkaCode/TTF"/* "$fontdir"
+            install "dist/IosevkaTerminal/TTF"/* "$fontdir"
+            runHook postInstall
+          '';
+        };
+      };
+
+      apps.default = utils.lib.mkApp {drv = self.packages.${system}.default;};
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [nodejs_23 ttfautohint];
+      };
+    });
+}
