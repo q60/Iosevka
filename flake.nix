fcb7227a930489d6334551c309a017ebfc6c5f99
kira malinova <llathasa@outlook.com>
2025-02-25 23:23:23 +0300

    push runtime closure instead

--- flake.nix
+++ flake.nix
@@ -24,9 +24,36 @@
     utils.lib.eachDefaultSystem
     (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) buildNpmPackage;
     in {
       packages = {
-        default = import ./default.nix {inherit pkgs;};
+        default = buildNpmPackage {
+          name = "iosevka-q60";
+
+          nativeBuildInputs = with pkgs; [
+            ttfautohint
+          ];
+
+          src = ./.;
+
+          npmDepsHash = "sha256-TOFCTYReO7eNBv1udG1Ie3Zww3uOVPFTo0x0jwyCAHE=";
+
+          buildPhase = ''
+            runHook preBuild
+            npm run build -- ttf::IosevkaCode --jCmd=6
+            npm run build -- ttf::IosevkaTerminal --jCmd=6
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            fontdir="$out/share/fonts/truetype"
+            install -d "$fontdir"
+            install "dist/IosevkaCode/TTF"/* "$fontdir"
+            install "dist/IosevkaTerminal/TTF"/* "$fontdir"
+            runHook postInstall
+          '';
+        };
       };
 
       apps.default = utils.lib.mkApp {drv = self.packages.${system}.default;};
